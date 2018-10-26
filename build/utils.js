8cf073ec255391f1f20b7034dcecfcfe7ea043f7
shimingsong <simgsg@163.com>
2018-10-26 17:38:57 +0800

    发布到githup

--- build/utils.js
+++ build/utils.js
@@ -47,7 +47,9 @@ exports.cssLoaders = function (options) {
     if (options.extract) {
       return ExtractTextPlugin.extract({
         use: loaders,
+        publicPath:'../../',   //添加这句
         fallback: 'vue-style-loader'
+
       })
     } else {
       return ['vue-style-loader'].concat(loaders)
