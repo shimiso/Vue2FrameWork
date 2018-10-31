45b4ef8bce9f5c0ea51ccaeea145f2dd7e9c4450
shimingsong <simgsg@163.com>
2018-10-31 15:12:47 +0800

    提交MescrollVue案例

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,11 @@ import ListView from '../views/ListView'
 import HomeView from '../views/HomeView'
 import TabitemView from '../views/TabitemView'
 import XunFeiTest from '../views/XunFeiTest'
-import LoadMore from '../views/LoadMore'
+import listNews from '../views/mescroll/list-news'
+import mescrollOptions from '../views/mescroll/mescroll-options'
+import listProducts from '../views/mescroll/list-products'
+import mescrollComponent from '../views/mescroll/mescroll-component'
+
 Vue.use(Router)
 
 export default new Router({
@@ -14,13 +18,11 @@ export default new Router({
       path: '/',
       name: 'HomeView',
       redirect: '/HomeView'
-    },
-    {
+    }, {
       path: '/HomeView',
       name: 'HomeView',
       component: HomeView
-    },
-    {
+    }, {
       path: '/TabbarView',
       name: 'TabbarView',
       component: TabbarView,
@@ -28,29 +30,35 @@ export default new Router({
         {
           path: '',
           redirect: '/TabbarView/TabitemView'
-        },
-        {
+        }, {
           path: 'TabitemView',
           name: 'TabitemView',
           component: TabitemView
         }]
-    },
-    {
+    }, {
       path: '/ListView',
       name: 'ListView',
       component: ListView
-    }
-    ,
-    {
+    }, {
       path: '/XunFeiTest',
       name: 'XunFeiTest',
       component: XunFeiTest
-    }
-    ,
-    {
-      path: '/LoadMore',
-      name: 'LoadMore',
-      component: LoadMore
+    }, {
+      path: '/listNews',
+      name: 'listNews',
+      component: listNews
+    }, {
+      path: '/mescrollOptions',
+      name: 'mescrollOptions',
+      component: mescrollOptions
+    }, {
+      path: '/listProducts',
+      name: 'listProducts',
+      component: listProducts
+    }, {
+      path: '/mescrollComponent',
+      name: 'mescrollComponent',
+      component: mescrollComponent
     }
   ]
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,32 +12,44 @@
       <cell title="科大讯飞" :link="{path:'/XunFeiTest'}" is-link></cell>
     </group>
 
-      <group title="上拉刷新，下拉加载">
-      <cell title="LoadMore" :link="{path:'/LoadMore'}" is-link></cell>
+    <group title="mescroll + vue 案例">
+      <cell title="list-news 新闻列表" :link="{path:'/listNews'}" is-link><span class="tip">下拉刷新添加数据到列表顶部</span></cell>
+      <cell title="list-products 商品列表" :link="{path:'/listProducts'}" is-link><span class="tip">下拉刷新重置列表数据</span></cell>
+      <cell title="mescroll-vue 组件" :link="{path:'/mescrollComponent'}" is-link><span class="tip" style="color:red">强烈推荐以组件的方式使用</span>
+      </cell>
+      <cell title="mescroll-options 所有配置项" :link="{path:'/mescrollOptions'}" is-link><span
+        class="tip">快速熟悉mescroll</span></cell>
     </group>
   </div>
 </template>
 
 <script>
-import { Group, Cell, TransferDomDirective as TransferDom } from 'vux'
+  import {Group, Cell, TransferDomDirective as TransferDom} from 'vux'
 
-export default {
-  directives: {
-    TransferDom
-  },
-  components: {
-    Group,
-    Cell
-  },
-  data () {
-    return {
+  export default {
+    directives: {
+      TransferDom
+    },
+    components: {
+      Group,
+      Cell
+    },
+    data() {
+      return {}
+    },
+    methods: {},
+    mounted() {
+    },
+    beforeDestroy() {
     }
-  },
-  methods: {
-  },
-  mounted () {
-  },
-  beforeDestroy () {
   }
-}
 </script>
+
+<style scoped>
+  .tip {
+    float: right;
+    margin-top: 2px;
+    font-size: 12px;
+    color: gray;
+  }
+</style>
