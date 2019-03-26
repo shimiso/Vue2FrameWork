a0b4550a94f7e7cd2b92d0205b2bb24421ecff9e
shimingsong <simgsg@163.com>
2019-03-26 18:14:00 +0800

    更新数据加载控件

--- src/store/modules/list.js
+++ src/store/modules/list.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 const state = {
-  items: [],
-  size: 3
+  items: []
 }
 const mutations = {
   getUpCallbackList (state, payload) {
@@ -10,43 +9,32 @@ const mutations = {
 }
 
 const actions = {
-  getUpCallbackList ({commit, state}, upParams) {
-    Vue.prototype.$http.get('/douban/event/list', {
-      params: {
-        'loc': '108288',
-        'start': 0,
-        'count': state.size
-      }
-    })
-      .then((response) => {
-        let len = response.data.events.length
-        if (len === 0) {
-          // 重写关于数据为空的dom
-          upParams.mescrollUp.empty = {
-            warpId: 'dataList', // 父布局的id;
-            icon: './static/mescroll/mescroll-empty.png', // 图标,支持网络图
-            tip: '暂无相关数据~', // 提示
-            btntext: '去逛逛 >', // 按钮,默认""
-            btnClick () { // 点击按钮的回调,默认null
-              // 数据为空点击btn的回调，或者直接不再这里重写，直接用listView的默认配置
-              alert('数据为空点击btn的回调')
-            }
+  getUpCallbackList ({commit, state},page) {
+    return new Promise((resolve, reject) => {
+      setTimeout(function () {
+        Vue.prototype.$http.get('/douban/event/list', {
+          params: {
+            'loc': '108288',
+            'start': page.num,
+            'count': page.size
           }
-        }
-        // Vue.prototype.showToast('请求成功')
-        commit({
-          type: 'getUpCallbackList',
-          res: response.data.events
+        }).then((response) => {
+          if(!response.data||!response.data.events){
+            reject(response.data)
+            return
+          }
+          commit({
+            type: 'getUpCallbackList',
+            res: response.data.events
+          })
+          resolve(response.data.events);
+          console.log('成功返回：', response.data)
+        }, (error) => {
+          reject(error)
         })
-        // 数据渲染成功后,隐藏下拉刷新的状态
-        upParams.mescroll.endSuccess(response.data.events.length)
-        console.log('成功返回：', response.data)
-      }, (error) => {
-        // 联网异常,隐藏上拉和下拉的加载进度
-        upParams.endErr()
-        // 弹窗提示或者请求错误页面
-        Vue.prototype.showToast('请求失败：' + error);
-      })
+      }, 1000);
+    })
+
   }
 }
 export default {
--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -3,97 +3,115 @@
     <x-header :left-options="{backText: ''}">ListViewDemo</x-header>
     <mescroll-vue ref="mescroll" :up="mescrollUp" @init="mescrollInit">
       <div class="list" id="dataList">
-          <template v-if="mold === 'thumbnail'" v-for="item in items" >
-            <router-link
-              class="thumbnail"
-              :to="{name: 'DetailView', params: { id: item.id }}">
-              <div class="content">
-                <img :src="item.image_hlarge" alt="cover">
-                <h3>{{item.title}}</h3>
-                <p>{{item.content | subStr}}</p>
-              </div>
-              <div class="author">
-                <span class="name">{{item.category_name}}</span>
-                <span class="label" v-if="item.subcategory_name">
+        <template v-if="mold === 'thumbnail'" v-for="item in items">
+          <router-link
+            class="thumbnail"
+            :to="{name: 'DetailView', params: { id: item.id }}">
+            <div class="content">
+              <img :src="item.image_hlarge" alt="cover">
+              <h3>{{item.title}}</h3>
+              <p>{{item.content | subStr}}</p>
+            </div>
+            <div class="author">
+              <span class="name">{{item.category_name}}</span>
+              <span class="label" v-if="item.subcategory_name">
                 本活动来自栏目 {{item.subcategory_name}}
               </span>
-              </div>
-            </router-link>
-          </template>
-          <template v-if="mold === 'basic'">
-            <ul class="basic">
-              <li v-for="item in items">
-                <a href="#">
-                  <h3>{{item.title}}</h3>
-                  <div class="info">{{item.comments}}</div>
-                </a>
-              </li>
-            </ul>
-          </template>
+            </div>
+          </router-link>
+        </template>
+        <template v-if="mold === 'basic'">
+          <ul class="basic">
+            <li v-for="item in items">
+              <a href="#">
+                <h3>{{item.title}}</h3>
+                <div class="info">{{item.comments}}</div>
+              </a>
+            </li>
+          </ul>
+        </template>
       </div>
     </mescroll-vue>
   </div>
 </template>
 
 <script>
-import MescrollVue from 'mescroll.js/mescroll.vue'
-import { XHeader } from 'vux'
-import { mapState, mapActions } from 'vuex'
-export default {
-  name: 'home-view',
-  components: { XHeader, MescrollVue },
-  data () {
-    var _this = this
+  import MescrollVue from 'mescroll.js/mescroll.vue'
+  import {XHeader} from 'vux'
+  import {mapState, mapActions} from 'vuex'
 
-    return {
-      mold: 'thumbnail',
-      mescroll: null,
-      mescrollUp: {
-        callback: this.upCallback, // 上拉加载回调函数
-        page: {
-          num: 0, // 当前页码,默认0,回调之前会加1,即callback(page)会从1开始
-          size: 3 // 每页展示的数据条数
-        },
-        empty: {
-          warpId: 'dataList', // 父布局的id;
-          icon: './static/mescroll/mescroll-empty.png', // 图标,支持网络图
-          tip: '暂无相关数据~', // 提示
-          btntext: '去逛逛 >', // 按钮,默认""
-          btnClick () { // 点击按钮的回调,默认null
-            // 请求失败后继续请求加载数据的接口
-            _this.upCallback(_this.mescrollUp, _this.mescroll) // 此处是点击去逛逛的时候重新请求上拉加载的方法
+  export default {
+    name: 'home-view',
+    components: {XHeader, MescrollVue},
+    data() {
+      return {
+        mold: 'thumbnail',
+        mescroll: null,
+        mescrollUp: {
+          callback: this.upCallback, // 上拉加载回调函数
+          page: {
+            num: 0, // 当前页码,默认0,回调之前会加1,即callback(page)会从1开始
+            size: 4 // 每页数据的数量
+          },
+          noMoreSize: 5, // 如果列表已无数据,可设置列表的总数量要大于等于5条才显示无更多数据;避免列表数据过少(比如只有一条数据),显示无更多数据会不好看
+          toTop: {
+            src: '../static/mescroll/mescroll-totop.png', // 回到顶部按钮的图片路径,支持网络图
+            offset: 500, // 列表滚动多少距离才显示回到顶部按钮,默认1000
+          },
+          empty: {
+            warpId: 'dataList', // 父布局的id;
+            icon: './static/mescroll/mescroll-empty.png', // 图标,支持网络图
+            tip: '暂无相关数据~', // 提示
+            btntext: '去逛逛 >', // 按钮,默认""
+            btnClick() { // 点击按钮的回调,默认null
+              // 请求失败后继续请求加载数据的接口
+              this.mescroll.resetUpScroll() // 刷新列表数据
+            }
           }
         }
       }
-    }
-  },
-  computed: {
-    ...mapState({
-      items: state => state.list.items
-    })
-  },
-  methods: {
-    mescrollInit (mescroll) {
-      this.mescroll = mescroll
     },
-    upCallback (mescrollUp, mescroll) {
-      let upParams = {
-        mescrollUp,
-        mescroll
+    computed: {
+      ...mapState({
+        items: state => state.list.items
+      })
+    },
+    methods: {
+      mescrollInit(mescroll) {
+        this.mescroll = mescroll
+      },
+      upCallback(page, mescroll) {
+        this.getUpCallbackList(page).then(response => {
+            // 数据渲染成功后,隐藏下拉刷新的状态
+            mescroll.endSuccess(response.length)
+          },
+          (error) => {
+            this.mescrollUp.empty = {
+              warpId: 'dataList', // 父布局的id;
+              icon: './static/mescroll/mescroll-error.png', // 图标,支持网络图
+              tip: '服务器发生未知异常', // 提示
+              btntext: '点击重试', // 按钮,默认""
+              btnClick() { // 点击按钮的回调,默认null
+                // 请求失败后继续请求加载数据的接口
+                mescroll.resetUpScroll() // 刷新列表数据
+              }
+            }
+            // 联网异常,隐藏上拉和下拉的加载进度
+            mescroll.endSuccess(0)
+            // _this.showToast('请求失败：' + error);
+          })
+      },
+      ...mapActions([
+        'getUpCallbackList'
+      ])
+    },
+    filters: {
+      subStr: function (value) {
+        let newVal = value.replace(/<.*?>/g, '')
+        return newVal.slice(0, 30)
       }
-      this.getUpCallbackList(upParams)
-    },
-    ...mapActions([
-      'getUpCallbackList'
-    ])
-  },
-  filters: {
-    subStr: function (value) {
-      let newVal = value.replace(/<.*?>/g, '')
-      return newVal.slice(0, 30)
     }
   }
-}
 </script>
 
 <style lang="scss" scoped>
@@ -101,6 +119,7 @@ export default {
     margin: 0 1.8rem;
     padding-top: 0.2rem;
   }
+
   .list {
     .mescroll {
       position: fixed;
