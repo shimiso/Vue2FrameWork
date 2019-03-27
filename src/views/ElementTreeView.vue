72a6207286e6fd542327bbd182a9ae8f055f0e5c
shimingsong <simgsg@163.com>
2019-03-27 17:29:17 +0800

    更新Tree控件

--- src/views/ElementTreeView.vue
+++ src/views/ElementTreeView.vue
@@ -9,9 +9,14 @@
 
       <div slot="drawer">
         <!-- 菜单内容 -->
-        <group title="请选择部门" style="margin-top:20px;">
-        <el-tree :data="data" :props="defaultProps" show-checkbox @check-change="handleCheckChange" @node-click="handleNodeClick"></el-tree>
+        <group title="请选择部门" style="margin-top:20px;height: max-content">
+          <el-tree :data="data" :props="defaultProps" show-checkbox @check-change="handleCheckChange" @node-click="handleNodeClick"></el-tree>
+          <flexbox justify="center" style="margin-top: 20px">
+            <flexbox-item style="text-align: center"><x-button mini @click.native="drawerVisibility = !drawerVisibility">取消</x-button></flexbox-item>
+            <flexbox-item style="text-align: center"><x-button mini type="primary" @click.native="drawerVisibility = !drawerVisibility">确定</x-button></flexbox-item>
+          </flexbox>
         </group>
+
       </div>
 
       <!-- main content -->
@@ -29,7 +34,7 @@
   </div>
 </template>
 <script>
-  import { Drawer,Group, Cell, XButton, ViewBox, XHeader } from 'vux'
+  import { Drawer,Group, Cell, XButton, ViewBox, XHeader,Flexbox, FlexboxItem } from 'vux'
 
   import { mapGetters } from 'vuex'
   export default {
@@ -39,7 +44,7 @@
       XHeader,
       Group,
       Cell,
-      XButton
+      XButton,Flexbox, FlexboxItem
     },
     data() {
       return {
