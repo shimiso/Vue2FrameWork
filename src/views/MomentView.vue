f3f3491a81007784753730dc7431291db6f853fd
shimingsong <simgsg@163.com>
2019-04-08 12:42:14 +0800

    时间格式化工具

--- src/views/MomentView.vue
+++ src/views/MomentView.vue
@@ -29,6 +29,21 @@ moment().format('LLL');  // 2019年4月8日中午11点55分
 moment().format('lll');  // 2019年4月8日中午11点55分
 moment().format('LLLL'); // 2019年4月8日星期一中午11点55分
 moment().format('llll'); // 2019年4月8日星期一中午11点55分
+
+什么是UTC时间？
+UTC是协调世界时(Universal Time Coordinated)英文缩写，是由国际无线电咨询委员会规定和推荐,
+并由国际时间局(BIH)负责保持的以秒为基础的时间标度。UTC相当于本初子午线(即经度0度)上的平均太阳时
+UTC + 时区差 ＝ 本地时间     中国的时间与UTC的时差均为+8，也就是UTC+8。
+Date: Fri, 08 Nov 2002 09:42:22 +0800
+
+
+什么是GMT时间？
+由于地球轨道并非圆形，其运行速度又随着地球与太阳的距离改变而出现变化，因此视太阳时欠缺均匀性。
+视太阳日的长度同时亦受到地球自转轴相对轨道面的倾斜度所影响。
+为着要纠正上述的不均匀性，天文学家计算地球非圆形轨迹与极轴倾斜对视太阳时的效应。
+平太阳时就是指经修订后的视太阳时。在格林尼治子午线上的平太阳时称为世界时(UT0)，又叫格林尼治平时(GMT)或者格林威治平时。
+Date: Wed, 25 Jul 2018 19:19:42 GMT-0400 (AST)
+
 -->
 <template>
   <div id="app">
@@ -39,6 +54,7 @@ moment().format('llll'); // 2019年4月8日星期一中午11点55分
       <p>完整版：{{dateString | dateformat}}</p>
       <p>年月日：{{dateString | dateformat('YYYY-MM-DD')}}</p>
       <p>时分秒：{{dateString | dateformat('HH:mm:ss')}}</p>
+      <p>UTC格式：{{UTCDate }}</p>
     </div>
   </div>
 </template>
@@ -63,6 +79,9 @@ moment().format('llll'); // 2019年4月8日星期一中午11点55分
     computed: {
       date:function(){
         return new Date();
+      },
+      UTCDate:function(){
+        return moment.utc([2010, 1, 14, 15, 25, 50, 125]);
       },
       // 计算属性的 getter
       dateString: function () {
